fe8f7dc3d62bc24b931191bdac1ce53a0bdd0bcc
taylanunutmaz <taylanunutmaz@gmail.com>
2019-04-02 19:03:29 +0300

    Dosya adı argüman olarak verilmemişse veya yanlış ise geçerli bir dosya adı girene kadar kullanıcıdan dosya adı isteme eklendi

--- DosyadakiHakikat/Program.cs
+++ DosyadakiHakikat/Program.cs
@@ -11,11 +11,23 @@ namespace DosyadakiHakikat
     {
         static void Main(string[] args)
         {
+            string dosyaAdi = args.Length > 0 ? args[0] : null;
+
+            // Geçerli bir dosya adı alana kadar dosya adı iste
+            while (!File.Exists(dosyaAdi))
+            {
+                Console.WriteLine("Dosya Adı Giriniz:");
+                dosyaAdi = Console.ReadLine();
+            }
+
+
+            StreamReader sr = File.OpenText(dosyaAdi);
+
             string s;
-            StreamReader sr = File.OpenText(args[0]);
-            while((s = sr.ReadLine()) != null)
-            Console.WriteLine(s);
+            while ((s = sr.ReadLine()) != null)
+                Console.WriteLine(s);
             Console.ReadLine();
+
         }
     }
 }
