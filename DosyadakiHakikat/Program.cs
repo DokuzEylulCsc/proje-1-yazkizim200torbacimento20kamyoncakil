714210d4241026fff7f734eebbc6b518c4ee1b00
taylanunutmaz <taylanunutmaz@gmail.com>
2019-04-03 19:38:38 +0300

    exception handling eklendi

--- DosyadakiHakikat/Program.cs
+++ DosyadakiHakikat/Program.cs
@@ -11,16 +11,37 @@ namespace DosyadakiHakikat
     {
         static void Main(string[] args)
         {
+            StreamReader sr;
+            bool dosyaGecerliMi = false;
             string dosyaAdi = args.Length > 0 ? args[0] : null;
             // Geçerli bir dosya adı alana kadar dosya adı iste
-            while (!File.Exists(dosyaAdi))
+            do
             {
-                Console.WriteLine("Dosya Adı Giriniz:");
-                dosyaAdi = Console.ReadLine();
-            }
+                try
+                {
+                    sr = File.OpenText(dosyaAdi);
+                    dosyaGecerliMi = true;
+                }
+                catch (FileNotFoundException )
+                {
+                    Console.WriteLine("Dosya Adı Giriniz:");
+                    dosyaAdi = Console.ReadLine();
+                }
+                catch (ArgumentNullException)
+                {
+                    Console.WriteLine("Dosya Adı Giriniz:");
+                    dosyaAdi = Console.ReadLine();
+                }
+                catch (ArgumentException)
+                {
+                    Console.WriteLine("Dosya Adı Giriniz:");
+                    dosyaAdi = Console.ReadLine();
+                }
 
+            } while (!dosyaGecerliMi);
 
-            StreamReader sr = File.OpenText(dosyaAdi);
+            sr = File.OpenText(dosyaAdi);
+
 
             string s;
             while ((s = sr.ReadLine()) != null)
