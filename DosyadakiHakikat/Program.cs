fbb8bdac7799d9198934e7d21bf7a77a99f9a755
sepkov <furkansonkaya61@gmail.com>
2019-04-03 20:26:55 +0300

    Log methodu tanımlandı ve gerekli yerlerde kullanıldı.

--- DosyadakiHakikat/Program.cs
+++ DosyadakiHakikat/Program.cs
@@ -13,7 +13,35 @@ namespace DosyadakiHakikat
         {
             StreamReader sr;
             bool dosyaGecerliMi = false;
-            string dosyaAdi = args.Length > 0 ? args[0] : null;
+            bool logMode = false;
+            string dosyaAdi;
+
+            void Log(string mesaj)
+            {
+                if (!logMode)
+                {
+                    StreamWriter sw = File.AppendText("log.txt");
+                    sw.WriteLine(mesaj);
+                    sw.Flush();
+                    sw.Close();
+                }
+            }
+
+            if (args.Length > 0)
+            {
+                if (args[0] == "-L")
+                {
+                    dosyaAdi = args.Length > 1 ? args[1] : null;
+                    logMode = true;
+                }
+                else
+                {
+                    dosyaAdi = args[0];
+                }
+            }
+            else dosyaAdi = null;
+            Log("Dosyadaki Hakikat V1.0 " + DateTime.Now.ToString() + " Tarihinde Başladı");
+
             // Geçerli bir dosya adı alana kadar dosya adı iste
             do
             {
@@ -21,19 +49,23 @@ namespace DosyadakiHakikat
                 {
                     sr = File.OpenText(dosyaAdi);
                     dosyaGecerliMi = true;
+                    Log(DateTime.Now + " tarihinde " + dosyaAdi + " isimli dosya başarıyla açıldı.");
                 }
                 catch (FileNotFoundException )
                 {
+                    Log(DateTime.Now + " tarihinde " + dosyaAdi + " dosya adı ile FileNotFoundException gerçekleşti.");
                     Console.WriteLine("Dosya Adı Giriniz:");
                     dosyaAdi = Console.ReadLine();
                 }
                 catch (ArgumentNullException)
                 {
+                    Log(DateTime.Now + " tarihinde " + dosyaAdi + " dosya adı ile ArgumentNullException gerçekleşti.");
                     Console.WriteLine("Dosya Adı Giriniz:");
                     dosyaAdi = Console.ReadLine();
                 }
                 catch (ArgumentException)
                 {
+                    Log(DateTime.Now + " tarihinde " + dosyaAdi + " dosya adı ile ArgumentException gerçekleşti.");
                     Console.WriteLine("Dosya Adı Giriniz:");
                     dosyaAdi = Console.ReadLine();
                 }
@@ -42,12 +74,13 @@ namespace DosyadakiHakikat
 
             sr = File.OpenText(dosyaAdi);
 
-
+            //Logun Ekrana Yazdırılması
             string s;
-            while ((s = sr.ReadLine()) != null)
-                Console.WriteLine(s);
-            Console.ReadLine();
+            while ((s = sr.ReadLine()) != null && logMode) Console.WriteLine(s);
 
+            Log("Program " + DateTime.Now.ToString() + " Tarihinde Sonlandı\n-----------------------------------------\n");
+            Console.WriteLine("Program Bitti Çıkmak İçin Enter'e Basınız");
+            Console.ReadLine();
         }
     }
 }
